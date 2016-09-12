70235e73eaf20348c54a2a51a6b27d0f132b7bf7
janyelson <janyelsonvictor@gmail.com>
2016-09-11 22:51:49 -0300

    atualizando

--- NewImage.m
+++ NewImage.m
@@ -5,9 +5,9 @@ function [ retorno ] = NewImage( img1, img2 )
         img = uint8(zeros(size(img1)));
         for i=1:size(img1,1)
             for j=1:size(img1,2)
-                img(i,j,1) =  (img1(i,j,1) + img2(i,j,1))/2;
-                img(i,j,2) =  (img1(i,j,2) + img2(i,j,2))/2;
-                img(i,j,3) =  (img1(i,j,3) + img2(i,j,3))/2;
+                img(i,j,1) =  (img1(i,j,1)/2 + img2(i,j,1)/2);
+                img(i,j,2) =  (img1(i,j,2)/2 + img2(i,j,2)/2);
+                img(i,j,3) =  (img1(i,j,3)/2 + img2(i,j,3)/2);
             end;    
         end;
         retorno = img;
